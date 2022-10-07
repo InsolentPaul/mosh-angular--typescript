065fb9dc39de04dee70a37d869ace9e542620916
InsolentPaul <pavlo.kravchenko@proton.me>
2022-10-07 23:37:28 +0300

    First commit

---
+++ src/app/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses', // <courses>
+  template: '<h2>Template test</h2>',
+}) // decorator function
+export class CoursesComponent {
+  // Pascal naming
+  //
+}
