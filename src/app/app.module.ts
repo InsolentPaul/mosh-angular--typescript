07a9d5a281a7957813e57ff4ebdf9b9e61d40db0
InsolentPaul <pavlo.kravchenko@proton.me>
2022-10-07 23:59:16 +0300

    generating components using Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent],
+  declarations: [AppComponent, CourseComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
